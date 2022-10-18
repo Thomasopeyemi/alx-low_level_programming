8337c69fd398d27158448f8e0cd014747c7bf213
Thomasopeyemi <Thomasopeyemi@rocketmail.com>
2022-10-18 16:00:08 -0700

    trial of 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints some text
+ *
+ * Return: returns 0
+ *
+ */
+
+void print_alphabet(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+
+}
