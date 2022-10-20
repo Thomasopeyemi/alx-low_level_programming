09cb5df36bfe443d0db25c0002726b2dbd1784a1
Thomasopeyemi <Thomasopeyemi@rocketmail.com>
2022-10-20 13:52:23 -0700

    zeroo

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -8,11 +8,16 @@
  *
  */
 
-int _isupper(int c)
+int main(void)
 {
-	if (c >= 'A' || c <= 'Z')
-		return (1);
-	else
-		return (0);
+	char c;
+
+	c = 'A';
+	_putchar("%c: %d\n", c, _isupper(c));
+
+	c = 'a';
+	_putchar("%c: %d\n", c, _isupper(c));
+
+	return (0);
 
 }
