0738435fcaf4b5fc9919a88762a480f410be4934
Thomasopeyemi <Thomasopeyemi@rocketmail.com>
2022-10-27 15:42:11 -0700

    testing task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses elements of an array
+ *
+ * @a: array in question
+ * @n: number of elements of the array
+ *
+ * Return: 0
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i;
+	int x;
+
+	for (i = 1; i < n; i++)
+	{
+		x = n - i;
+		x++;
+	}
+
+	while (a[x] != 0)
+	{
+		x++;
+	}
+
+
+}
