957b5ce0ebb07fee09af91cfcb2d3206f6209a3c
Thomasopeyemi <Thomasopeyemi@rocketmail.com>
2022-10-27 14:55:00 -0700

    fix 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,11 +12,17 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+	int i = 0;
 
 	for (i = 0; i < n; i++)
 	{
 		dest[i] = src[i];
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
 	}
 
 	return (dest);
